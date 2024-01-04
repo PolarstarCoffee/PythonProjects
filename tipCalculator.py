7d993758b12b33ab1c2ac01317cce3b6b8c11400
Zuriel Zavala <98419830+PolarstarCoffee@users.noreply.github.com>
2024-01-04 00:27:46 -0600

    Add files via upload

---
+++ tipCalculator.py
@@ -0,0 +1,11 @@
+print("Welcome to this lil' tip calculator! Don't worry, we tip our waiters and waitresses around here.")
+total = input("So, how much was the damage?\n")
+party = int(input("How many people is it split by?\n"))
+splitCost = float(total)/float(party)
+tip = int(input("How much do you plan on tipping percentage wise?\n"))
+tipConvert = float(tip) / 100
+totalWTip = float(total) * float(tip)
+totalPerPersonTipped = float(splitCost) * float(tipConvert) + float(splitCost)
+EndProduct = round(totalPerPersonTipped,2)
+print(f"Each person should pay: {EndProduct}$")
+
